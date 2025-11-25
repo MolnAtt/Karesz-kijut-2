c56994af323e51f87ea0ec61c613d6eebe79c3ee
Molnár Attila <molnar.attila@szlgbp.hu>
2025-11-25 07:59:28 +0100

    Add project files.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Karesz kijut I.
+Karesz álljon ki a pálya szélére és szívjon egy kis friss levegőt! Ha elér a pálya szélére, 
+akkor mondjon valamit, ami tartalmazza azt a stringet, hogy "Milyen szép a kilátás!"
+
+A pálya széle véletlenszerűen van elhelyezve a pálya sarkaiban,
+ahogy Karesz kezdeti pozíciója és iránya is véletlenszerű. 
+
+Ebben a feladatban **nem lehet ultrahang szenzorokat használni!**
+
+
+# K.A.R.E.SZ.
+A Középiskolai Algoritmusokra Rávezető Edukációs Szoftver (továbbiakban K.A.R.E.SZ.) 
+a [Karel nyelv](https://en.wikipedia.org/wiki/Karel_(programming_language) egy C# nyelven történő implementációja, 
+amely párhuzamos programozási eszközökkel teszi lehetővé a programozás középiskolai oktatását. 
+A Kőbányai Szent László Gimnázium informatika tagozatán való alkalmazása Nagy Tibor munkássága óta hagyomány. 
+
---
+++ Tanar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Karesz
+{
+	public partial class Form1 : Form
+	{
+		static Random r = new Random();
+		string betöltendő_pálya = "palya01.txt";
+		void TANÁR_ROBOTJAI()
+		{
+			Robot karesz = new Robot("Karesz", 1000, 1000, 1000, 1000, 0, r.Next(1,39), r.Next(1,29), r.Next(4), false, false);
+			Betölt(betöltendő_pálya);
+			Vektor[] kijáratok = new Vektor[]
+			{
+				new Vektor(0, 1),
+				new Vektor(1, 0),
+				new Vektor(39, 0),
+				new Vektor(40, 1),
+				new Vektor(1, 30),
+				new Vektor(0, 29),
+				new Vektor(39, 30),
+				new Vektor(40, 29),
+			};
+			Vektor kijárat = kijáratok[r.Next(8)];
+			pálya[kijárat] = 0;
+			Frissít();
+
+			//Mondd(kijárat.ToString());
+			//Mondd(pálya[kijárat].ToString());
+
+			//Robot lilesz = new Robot("Lilesz", 1000, 1000, 1000, 1000, 0, 10, 22, 0);
+			//lilesz.Feladat = delegate()
+			//{
+			//lilesz.Lépj();
+			//    lilesz.Lépj();
+			//};
+		}
+	}
+}
